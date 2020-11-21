30dd50a69e39ec04f24a08e37ba79a1360c5a17c
Huda Yousif <hjoski@gmail.com>
2020-11-21 11:23:43 -0800

    create migrations and seeds

--- wholesomeness-api/db/seeds.rb
+++ wholesomeness-api/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Activity.create(name:"swimming")
+Activity.create(name:"running")
+Activity.create(name:"hiking")
+Activity.create(name:"jogging")
+Activity.create(name:"reading")
+Activity.create(name:"mediating")
