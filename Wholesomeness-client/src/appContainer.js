56a762bddaa23d1465a3d192dfe06e6a5229e4b9
Huda Yousif <hjoski@gmail.com>
2020-11-28 09:49:53 -0800

    start getRandomActivities

--- Wholesomeness-client/src/appContainer.js
+++ Wholesomeness-client/src/appContainer.js
@@ -1,11 +1,22 @@
 class AppContainer {
-    activities = []
+    static activities = []
     categories = []
     url = "http://localhost:3000"
     routinePractice = {}
 
     bindEventListeners() {
         const btn = document.getElementById('createRoutinePractice');
+        btn.addEventListener('click', this.getRandomActivities)
+    }
+
+    getRandomActivities() {
+        debugger
+        let randomActivities = [];
+        for (let i = 0; i < 4; i++) {
+            debugger
+            randomActivities.push(this.activities[Math.floor(Math.random()*this.activities.length)])
+        };
+        return randomActivities;
     }
 
     getActivities() {
